88e34548e0bb46017777c07fb91e128386efc72a
CJ <875277780@qq.com>
2020-09-23 16:22:53 +0800

    9.23

--- Week_03/全排列.py
+++ Week_03/全排列.py
@@ -2,21 +2,18 @@ class Solution:
     def permute(self, nums):
         ans = []
         used = [False for _ in range(len(nums))]
-        nums.sort()
+
         def solve(cur):
             if len(cur) == len(nums):
-                ans.append(cur[:])
-                return
+                ans.append(cur)
             for i in range(len(nums)):
-                if used[i] == True or (i > 0 and nums[i] == nums[i - 1] and not used[i-1]):
+                if used[i] == True:
                     continue
                 used[i] = True
-                solve(cur + [nums[i]])
-                used[i] = False
 
+                solve(cur+[nums[i]])
+                used[i] = False
         solve([])
         return ans
-
-
 s = Solution()
-print(s.permute([1, 1,1,1, 2]))
+print(s.permute([1,2,3]))
